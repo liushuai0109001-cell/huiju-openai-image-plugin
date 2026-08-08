089947199acc4110d1e3672308bd63ba87e01cb1
liushuai0109001-cell <liushuai0109001-cell@users.noreply.github.com>
2026-08-09 01:29:03 +0800

    Fix image plugin UI encoding

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from plugin_utils import load_plugin_config, update_plugin_params
 
 _PLUGIN_FILE = __file__
-_PLUGIN_VERSION = "1.2.6"
+_PLUGIN_VERSION = "1.2.7"
 
 _FACE_CASCADE = None
 _EYE_CASCADE = None
